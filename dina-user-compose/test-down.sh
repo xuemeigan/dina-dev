204c61eaa3225066a52615272ad2fcc3a9bcbb2d
xuemeigan <xuemei.gan@canada.ca>
2020-12-04 20:09:59 -0500

    - Remove test related yml files as currenlty using vscode test runner/container to run and debug tests

--- dina-user-compose/test-down.sh
+++
@@ -1,8 +0,0 @@
-# Short-hand way of running docker-compose with the test launch config.
-
-export UID=$(id -u)
-export GID=$(id -g)
-
-docker-compose -p  dina-agent-api-test -f ./docker-compose.base.yml -f ./docker-compose.test.yml $@ down
-
-
