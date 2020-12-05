f70a04a2cd81f91a0f80ada73a83529d67f30b8f
xuemeigan <xuemei.gan@canada.ca>
2020-12-04 20:08:21 -0500

    - Remove object store api test related yml files  as currently using vscode run and debug test with testcontainer

--- objectstore-api-compose/test-osa-down.sh
+++
@@ -1,7 +0,0 @@
-# Short-hand way of running docker-compose with the test launch config.
-
-export UID=$(id -u)
-export GID=$(id -g)
-
-docker-compose -p dina-object-store-api-osa-test -f ./docker-compose.base-osa-test.yml -f ./docker-compose.test-osa.yml $@ down
-
