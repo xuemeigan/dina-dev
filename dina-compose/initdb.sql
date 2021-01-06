25a4054cfe08fc0e4d7afa01f5242ffa522df4a7
xuemeigan <xuemei.gan@canada.ca>
2021-01-06 15:26:34 -0500

    - Add collection api and update the env file for ip

--- dina-compose/initdb.sql
+++ dina-compose/initdb.sql
@@ -3,6 +3,6 @@ CREATE SCHEMA seqdb;
 CREATE SCHEMA keycloak;
 CREATE SCHEMA agent;
 CREATE SCHEMA userapi;
-
+CREATE SCHEMA collection;
 set schema 'seqdb';
 create extension if not exists "pgcrypto";
